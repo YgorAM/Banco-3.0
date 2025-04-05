e0f75440dbd32a58525928381fe54253e7c36b73
Ygor Amaral <ygoramaral30@gmail.com>
2025-04-04 21:58:25 -0300

    commit

--- src/Clientes.java
+++ src/Clientes.java
@@ -13,21 +13,23 @@ class Clientes {
     }
 
     public static void login(ArrayList<Clientes> contas) {
-        String usuarioM = JOptionPane.showInputDialog(null, "Insira seu usuário:");
-        String senhaM = JOptionPane.showInputDialog(null, "Insira sua senha:");
-        try {
-            int senhac = Integer.parseInt(senhaM);
+        while (true) {
+            String usuarioM = JOptionPane.showInputDialog(null, "Insira seu usuário:");
+            String senhaM = JOptionPane.showInputDialog(null, "Insira sua senha:");
+            try {
+                int senhac = Integer.parseInt(senhaM);
 
-            for (Clientes conta : contas) {
-                if (usuarioM.equals(conta.usuario) && senhac == conta.senhaA) {
-                    JOptionPane.showMessageDialog(null, "Login bem-sucedido!\nBem-vindo, " + conta.usuario);
-                    conta.menuPrincipal();
-                    return;
+                for (Clientes conta : contas) {
+                    if (usuarioM.equals(conta.usuario) && senhac == conta.senhaA) {
+                        JOptionPane.showMessageDialog(null, "Login bem-sucedido!\nBem-vindo, " + conta.usuario);
+                        conta.menuPrincipal();
+                        return;
+                    }
                 }
+                JOptionPane.showMessageDialog(null, "Usuário ou senha incorretos!");
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Senha deve ser um número válido!", "Erro", JOptionPane.ERROR_MESSAGE);
             }
-            JOptionPane.showMessageDialog(null, "Usuário ou senha incorretos!");
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Senha deve ser um número válido!", "Erro", JOptionPane.ERROR_MESSAGE);
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,23 @@
 import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
         ArrayList<Clientes> contas = new ArrayList<>();
 
-        Clientes conta1 = new Clientes("Ygor", 405730);
-        Clientes conta2 = new Clientes("Ana", 123456);
-        Clientes conta3 = new Clientes("Carlos", 987654);
+        Clientes conta1 = new Clientes("Ygor", 101010);
+        Clientes conta2 = new Clientes("Ana Clara", 202020);
+        Clientes conta3 = new Clientes("Natan", 303030);
+        Clientes conta4 = new Clientes("Nathan", 404040);
+        Clientes conta5 = new Clientes("Guilherme", 505050);
+        Clientes conta6 = new Clientes("Augusto", 606060);
 
         contas.add(conta1);
         contas.add(conta2);
         contas.add(conta3);
+        contas.add(conta4);
+        contas.add(conta5);
+        contas.add(conta6);
 
         Clientes.login(contas);
     }
